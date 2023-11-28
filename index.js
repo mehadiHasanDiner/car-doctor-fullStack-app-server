ace44721b1a8366e1a0b29443540a6dbb6ad1bc7
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-11-29 02:53:27 +0600

    module 71-6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -23,6 +24,25 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyJWT = (req, res, next) => {
+  console.log(req.headers.authorization);
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Invalid authorization" });
+  }
+  const token = authorization.split(" ")[1];
+  console.log("token inside authorization", token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(403).send({ error: true, message: "unauthorized" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -30,6 +50,17 @@ async function run() {
 
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
+
+    // JWT
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      console.log(token);
+      res.send({ token });
+    });
 
     // load all services data from
     app.get("/services", async (req, res) => {
@@ -61,8 +92,10 @@ async function run() {
     });
 
     // getting some data of the service collection database
-    app.get("/bookings", async (req, res) => {
-      console.log(req.query.email);
+    app.get("/bookings", verifyJWT, async (req, res) => {
+      // console.log(req.headers.authorization);
+      console.log("came back after verifying authorization");
+
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
